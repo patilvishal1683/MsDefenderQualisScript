2f8e1ebdf646597bbfd6e1440872d2c6e7809f87
patilvishal1683 <vishal.patil@bakerhughes.com>
2022-10-31 09:33:59 +0530

    initial commit

---
+++ script.sh
@@ -0,0 +1,76 @@
+#! /bin/bash
+sudo yum update
+file='/etc/yum.repos.d'
+
+cat << EOF > $file
+[microsoft-prod]
+name=Microsoft Defender for Endpoint
+baseurl=https://packages.microsoft.com/rhel/7/prod/
+enabled=1
+fastestmirror_enabled=0
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+
+sudo yum install mdatp
+
+config_file = '/etc/opt/microsoft/mdatp/managed/mdatp_managed.json'
+
+cat << EOF > $config_file
+{
+   "antivirusEngine":{
+      "enableRealTimeProtection":true,
+      "passiveMode":false,
+      "exclusionsMergePolicy":"merge",
+      "exclusions":[
+         {
+            "$type":"excludedPath",
+            "isDirectory":false,
+            "path":"/var/log/system.log"
+         },
+         {
+            "$type":"excludedPath",
+            "isDirectory":true,
+            "path":"/home"
+         },
+         {
+            "$type":"excludedFileExtension",
+            "extension":"pdf"
+         },
+         {
+            "$type":"excludedFileName",
+            "name":"cat"
+         }
+      ],
+      "allowedThreats":[
+         "EICAR-Test-File (not a virus)"
+      ],
+      "disallowedThreatActions":[
+         "allow",
+         "restore"
+      ],
+      "threatTypeSettingsMergePolicy":"merge",
+      "threatTypeSettings":[
+         {
+            "key":"potentially_unwanted_application",
+            "value":"block"
+         },
+         {
+            "key":"archive_bomb",
+            "value":"audit"
+         }
+      ]
+   },
+   "cloudService":{
+      "enabled":true,
+      "diagnosticLevel":"optional",
+      "automaticSampleSubmissionConsent":"safe",
+      "automaticDefinitionUpdateEnabled":true
+   }
+}
+EOF
+sudo apt install python3.8
+sudo apt install wget -y
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/linux/amd64/latest/MicrosoftDefenderATPOnboardingLinuxServer.py
+py3 MicrosoftDefenderATPOnboardingLinuxServer.py
+
