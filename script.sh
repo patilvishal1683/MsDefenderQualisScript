a76affa7edbba3529a3612406df7474310fc4b03
patilvishal1683 <vishal.patil@bakerhughes.com>
2022-11-09 14:20:00 +0530

    Added cloudwatch agent

--- script.sh
+++ script.sh
@@ -121,3 +121,8 @@ else
    echo "Something Went Wrong Unable to connect to curl request."
    kill $!
 fi
+
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/linux/amd64/latest/AmazonCloudWatchAgent.zip
+unzip AmazonCloudWatchAgent.zip
+sudo ./install.sh
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ssm:/alarm/AWS-CWAgentConfig -s
